c1f4e125b654094a3fa93c9a559fa064b8d345a2
Johnny Gérard <john@jgerard.dev>
2024-12-14 16:41:46 +0100

    build: install and configure AngularFire

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,13 +4,25 @@ import {
   withEventReplay,
 } from "@angular/platform-browser";
 import { provideRouter } from "@angular/router";
-
 import { routes } from "./app.routes";
+import { initializeApp, provideFirebaseApp } from "@angular/fire/app";
+import { getAuth, provideAuth } from "@angular/fire/auth";
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideClientHydration(withEventReplay()),
+    provideFirebaseApp(() =>
+      initializeApp({
+        projectId: "angularfire-authentication",
+        appId: "1:922614773049:web:36ccaf0879fe4f163d24e8",
+        storageBucket: "angularfire-authentication.firebasestorage.app",
+        apiKey: "AIzaSyBJd0EwwuZzciybBqvcqn6TsWGbTCVauuc",
+        authDomain: "angularfire-authentication.firebaseapp.com",
+        messagingSenderId: "922614773049",
+      }),
+    ),
+    provideAuth(() => getAuth()),
   ],
 };
