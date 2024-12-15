2c337427b992eb0f07c8708f82aa8e8cb8125a26
Johnny Gérard <john@jgerard.dev>
2024-12-16 00:30:34 +0100

    feat: add icons to NavbarComponent

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,15 +1,18 @@
 import { ChangeDetectionStrategy, Component, inject } from "@angular/core";
 import { RouterLink } from "@angular/router";
+import { House, LogOut, LucideAngularModule } from "lucide-angular";
 import { AuthService } from "../../services/auth.service";
 
 @Component({
   selector: "app-navbar",
-  imports: [RouterLink],
+  imports: [LucideAngularModule, RouterLink],
   templateUrl: "./navbar.component.html",
   host: { class: "block" },
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class NavbarComponent {
+  readonly I_HOUSE = House;
+  readonly I_LOG_OUT = LogOut;
   #auth = inject(AuthService);
   user = this.#auth.user;
 
