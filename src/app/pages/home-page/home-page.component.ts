4b84d3526b8a89abd16a5247a6dae1e1bd017261
Johnny Gérard <john@jgerard.dev>
2024-12-16 00:45:57 +0100

    feat: implement HomePageComponent

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,10 +1,28 @@
-import { ChangeDetectionStrategy, Component } from "@angular/core";
+import { ChangeDetectionStrategy, Component, inject } from "@angular/core";
+import { RouterLink } from "@angular/router";
+import {
+  Layout,
+  LogIn,
+  LucideAngularModule,
+  UserPlus,
+  Users,
+} from "lucide-angular";
+import { AuthService } from "../../services/auth.service";
 
 @Component({
   selector: "app-home-page",
-  imports: [],
+  standalone: true,
+  imports: [LucideAngularModule, RouterLink],
   templateUrl: "./home-page.component.html",
   host: { class: "block" },
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class HomePageComponent {}
+export class HomePageComponent {
+  readonly I_LAYOUT = Layout;
+  readonly I_USERS = Users;
+  readonly I_LOG_IN = LogIn;
+  readonly I_USER_PLUS = UserPlus;
+
+  #auth = inject(AuthService);
+  user = this.#auth.user;
+}
