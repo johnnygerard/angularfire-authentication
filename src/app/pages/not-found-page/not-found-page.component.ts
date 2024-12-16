1b8c03d8232a122cbe299f63853e0af9475b99b5
Johnny Gérard <john@jgerard.dev>
2024-12-16 04:30:04 +0100

    feat: reimplement NotFoundPageComponent

--- src/app/pages/not-found-page/not-found-page.component.ts
+++ src/app/pages/not-found-page/not-found-page.component.ts
@@ -1,11 +1,20 @@
 import { ChangeDetectionStrategy, Component } from "@angular/core";
 import { RouterLink } from "@angular/router";
+import { ArrowLeft, Home, LucideAngularModule } from "lucide-angular";
 
 @Component({
   selector: "app-not-found-page",
-  imports: [RouterLink],
+  standalone: true,
+  imports: [LucideAngularModule, RouterLink],
   templateUrl: "./not-found-page.component.html",
   host: { class: "block" },
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class NotFoundPageComponent {}
+export class NotFoundPageComponent {
+  readonly I_HOME = Home;
+  readonly I_ARROW_LEFT = ArrowLeft;
+
+  goBack(): void {
+    window.history.back();
+  }
+}
