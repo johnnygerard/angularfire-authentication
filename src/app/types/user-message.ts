456d77904770a071f5fbd4b4ef73a28c00c5f36a
Johnny Gérard <john@jgerard.dev>
2024-12-18 10:31:17 +0100

    style: sort properties in user-message.ts

--- src/app/types/user-message.ts
+++ src/app/types/user-message.ts
@@ -1,11 +1,11 @@
 export const USER_MESSAGE = {
   EMAIL_EXISTS:
     "This email address is already in use. Please sign in or use a different email address.",
+  INVALID_LOGIN_CREDENTIALS: "Invalid email or password. Please try again.",
   LOGIN_SUCCESS: "Welcome back! You are now signed in.",
   LOGOUT_SUCCESS: "You’ve successfully signed out. Have a nice day!",
   REGISTRATION_SUCCESS: "Your account has been created. Welcome aboard!",
   UNKNOWN_ERROR: "An unknown error has occurred. Please try again later.",
-  INVALID_LOGIN_CREDENTIALS: "Invalid email or password. Please try again.",
 } as const;
 
 export type UserMessage = (typeof USER_MESSAGE)[keyof typeof USER_MESSAGE];
